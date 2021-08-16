9255a277c6ba7a9509507a4d885077ee5b2781a2
Shubham Pitekar <65482186+its-shubham2323@users.noreply.github.com>
2021-08-16 08:45:06 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
